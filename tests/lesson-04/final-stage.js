37536178deeb3a0d2ad7bd4c8a1724c783d451e5
Cong Pham <phamduccong1234@gmail.com>
2026-05-26 23:23:45 +0700

    fix: change condition for loop

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,7 +1,7 @@
 let count = 0;
 function findPairsDivisibleBy17() {
-  for (let i = 1; i < 100; i++) {
-    for (let j = 1; j < 100; j++) {
+  for (let i = 1; i <= 100; i++) {
+    for (let j = i; j <= 100; j++) {
       if ((i + j) % 17 === 0) {
         console.log(`Pair: ${i} + ${j} = ${i + j}`);
         count++;
